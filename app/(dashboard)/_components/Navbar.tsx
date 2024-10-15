06c1f19460acfcdb78d137b1834ffd5266a8a78b
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-15 12:06:24 +0200

    Navbar + MobileSidebar

---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { NavbarRoutes } from "@/components/ui/NavbarRoutes"
+import { MobileSidebar } from "./MobileSidebar"
+
+export const Navbar = () => {
+    return (<div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+        <MobileSidebar/>
+        <NavbarRoutes/>
+    </div>)
+}
