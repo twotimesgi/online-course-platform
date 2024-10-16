5ed475abd0942da253b226df3b4f7700dfb326cd
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-16 17:38:49 +0200

    ChapterAccess form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterAccessForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormMessage,
+  FormItem,
+  FormDescription,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import {toast} from "sonner";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Chapter } from "@prisma/client";
+import { Editor } from "@/components/Editor";
+import { Preview } from "@/components/Preview";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface ChapterAccessFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  isFree: z.boolean().default(false)
+});
+
+export const ChapterAccessForm = ({ initialData, courseId, chapterId }: ChapterAccessFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((current) => !current);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      isFree: !!initialData.isFree
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const router = useRouter();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try{
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+        toast.success("Chapter updated.");
+        setIsEditing(false)
+        router.refresh();
+    }catch(error){
+        console.log(error)
+        toast.error("Something went wrong. Try again.")
+    }
+  };
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Visibility
+        <Button onClick={toggleEdit} variant={"ghost"}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2 " />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <div className={
+          cn(
+            "text-sm mt-2",
+            !initialData.isFree && "text-slate-500 italic"
+          )
+        }>
+          {initialData.isFree ? (
+            <>This chapter is available for preview.</>
+          ) : (<>Access to this chapter is restricted to course purchasers.</>)}
+          </div>
+      )}
+      {isEditing && (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+                <FormField
+                control={form.control}
+                name="isFree"
+                render={({field}) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                        <FormControl>
+                            <Checkbox checked={field.value} onCheckedChange={field.onChange}/>
+                        </FormControl>
+                        <div className="space-y-1 leading-none">
+                          <FormDescription>
+                            Check this box if you want to make this chapter free for preview.
+                          </FormDescription>
+                        </div>
+                        <FormMessage/>
+                    </FormItem>
+                )}/>
+                <div className="flex items-center gap-x-2">
+                    <Button
+                    disabled={!isValid || isSubmitting} type="submit">
+                        Save
+                    </Button>
+                </div>
+            </form>
+        </Form>
+      )}
+    </div>
+  );
+};
