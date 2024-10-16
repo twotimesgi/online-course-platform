78a204fdad19f0c8f860b7f9269afc1e774c1487
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-16 11:57:00 +0200

    Attachments Form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttachmentForm.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { File, Loader2, Pencil, PlusCircle, X } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Attachment, Course } from "@prisma/client";
+import { FileUpload } from "@/components/FileUpload";
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  url: z.string().min(1),
+});
+
+export const AttachmentForm = ({
+  initialData,
+  courseId,
+}: AttachmentFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values);
+      toast.success("Course updated.");
+      setIsEditing(false);
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong. Try again.");
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success("Attachment deleted");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong. Try again.");
+    } finally {
+      setDeletingId(null);
+    }
+  };
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Attachments
+        <Button onClick={toggleEdit} variant={"ghost"}>
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add file
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
+            <>
+              <Pencil className="h-4 w-4 mr-2 " />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className="text-sm italic mt-2 text-slate-500">No attachments</p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => {
+                return (
+                  <div
+                    key={attachment.id}
+                    className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                  >
+                    <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                    <p className="text-xs line-clamp-1">
+                      <a className="line-clamp-1" href={attachment.url} target="_blank">
+                        {attachment.name}
+                      </a>
+                    </p>
+                    {deletingId === attachment.id && (
+                      <div className="ml-auto">
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      </div>
+                    )}
+                    {deletingId !== attachment.id && (
+                      <button
+                        onClick={() => onDelete(attachment.id)}
+                        className="ml-auto hover:opacity-75"
+                      >
+                        <X className="h-4 w-4" />
+                      </button>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseAttachment"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ url: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            Add anything your students might need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
