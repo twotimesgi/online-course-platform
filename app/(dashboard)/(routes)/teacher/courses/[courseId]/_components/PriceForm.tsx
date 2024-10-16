12ddeac2bd3e190ad3901bf20d713c863a8ea1f7
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-16 15:34:58 +0200

    Fix form title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/PriceForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/PriceForm.tsx
@@ -57,7 +57,7 @@ export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Description
+        Price
         <Button onClick={toggleEdit} variant={"ghost"}>
           {isEditing ? (
             <>Cancel</>
