fdbde7182f2c9b1a4c6f1517075ea7dc62184006
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-16 18:32:38 +0200

    Video Upload and Player

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,35 +1,73 @@
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
+
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
 export async function PATCH(
-    req: Request,
-    {params} : {params: {courseId: string, chapterId: string}}
-){
-    try{
-        const {userId} = auth();
-        const {isPublished, ...values} = await req.json();
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await req.json();
 
-        if(!userId) return new NextResponse("Unauthorized", {status: 401})
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-        const courseOwner = await db.course.findUnique({where:{
-            id: params.courseId,
-            userId: userId
-        }});
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
 
-        if(!courseOwner) return new NextResponse("Unauthorized", {status: 401});
+    if (!courseOwner) return new NextResponse("Unauthorized", { status: 401 });
 
-        const chapter = await db.chapter.update({where:{
-            id: params.chapterId,
-            courseId: params.courseId
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
         },
-    data: {
-        ...values
-    }})
+      });
 
-    //TODO: hadle video upload
-    return  NextResponse.json(chapter)
-    }catch(error){
-        console.log("[CHAPTERPUT]", error)
-        return new NextResponse("Internal Error", {status: 500})
+      if (existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await video.assets.create({
+        input: values.videoUrl,
+        playback_policy: ["public"],
+        test: false
+      });
+      await db.muxData.create({
+        data: {
+            chapterId: params.chapterId,
+            assetId: asset.id,
+            playbackId: asset.playback_ids?.[0].id
+        }
+      })
     }
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[CHAPTERPUT]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
